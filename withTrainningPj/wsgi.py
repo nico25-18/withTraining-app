c5691e871245089c794d9ec7008226e68c2be901
松田 弦樹 <matsuda@nets.local>
2025-09-30 14:46:39 +0900

    静的フォルダへのアクセス対応

--- withTrainningPj/wsgi.py
+++ withTrainningPj/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'withTrainningPj.settings')
 
 application = get_wsgi_application()
+
+# BASE_DIR = プロジェクトのルートディレクトリ
+BASE_DIR = Path(__file__).resolve().parent.parent
+# 正しい staticfiles パスを指定
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
